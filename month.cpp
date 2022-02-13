007a20673ec92127f054679f94f0d97118d85f05
Sakib Khan <37256061+SakibKhan1@users.noreply.github.com>
2022-02-12 20:50:18 -0500

    Update month.cpp

--- month.cpp
+++ month.cpp
@@ -18,10 +18,12 @@ int main()
     cout << "Enter month: ";
     cin >> m;
     if(m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12){
+    //all months with 31 days
         cout << "31 days" << endl;
     }else if(m == 4 || m == 6 || m == 9 || m == 11){
+    //all months with 30 days
         cout << "30 days" << endl;
-    }else if(m == 2 && y % 4 == 0){
+    }else if(m == 2 && y % 4 == 0 && y % 100 == 0 && y % 400 == 0){
         cout << "29 days" << endl;
     }else if(m == 2 && y % 4!= 0){
         cout << "28 days" << endl;
